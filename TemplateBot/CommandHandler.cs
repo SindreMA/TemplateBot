14f954558d33476022c2815ed64039493483f164
SindreMA <sindrema@gmail.com>
2017-07-21 09:18:15 +0200

    Add files via upload

---
+++ TemplateBot/CommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Discord;
+using System.Collections.Generic;
+using System.Text;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TemplateBot
+{
+    class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _service;
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += _client_MessageReceived;
+        }
+
+        private async Task _client_MessageReceived(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+            int argPost = 0;
+            if (msg.HasCharPrefix('.',ref argPost))
+            {
+                var result = _service.ExecuteAsync(context, argPost);
+                if (!result.Result.IsSuccess && result.Result.Error != CommandError.UnknownCommand)
+                {
+                    await context.Channel.SendMessageAsync(result.Result.ErrorReason);
+                }
+            }
+        }
+    }
+}
---
+++ TemplateBot/Modules/Commands.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Ping")]
+        public async Task Ping()
+        {
+            await Context.Channel.SendMessageAsync("Pong!");
+        }
+
+    }
+}
